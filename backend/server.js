22cabdfb4c33460ba2d4f07a204d70e2caa404aa
ifr <ignacio.f.reggiani@gmail.com>
2025-08-29 16:29:13 -0300

    Set tab title; add security

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 require('dotenv').config();
 const { sequelize } = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
