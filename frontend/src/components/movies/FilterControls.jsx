4bc281b330f8bdbac700d61e65480ab2239d4fc3
ifr <ignacio.f.reggiani@gmail.com>
2025-09-15 13:24:59 -0300

    Style changes, filter controls layout

--- frontend/src/components/movies/FilterControls.jsx
+++ frontend/src/components/movies/FilterControls.jsx
@@ -4,26 +4,30 @@ import "../../styles/movies/_filter-controls.scss";
 const FilterControls = ({ ...props }) => {
   return (
     <div className="filter-controls">
-      <input
-        type="text"
-        value={props.searchTerm}
-        onChange={(e) => props.setSearchTerm(e.target.value)}
-        placeholder="Search by title..."
-        className="movie-table-input"
-      />
+      <div className="filter-input-group">
+        <label htmlFor="filter-by-name">Title:</label>
+        <input
+          id="filter-by-name"
+          type="text"
+          value={props.searchTerm}
+          onChange={(e) => props.setSearchTerm(e.target.value)}
+          placeholder="Search by title..."
+          className="movie-table-input"
+        />
+      </div>
       <input
         type="number"
         value={props.minYear}
         onChange={(e) => props.setMinYear(e.target.value)}
         placeholder="Min Year"
-        className="movie-table-input"
+        className="movie-table-input year-input"
       />
       <input
         type="number"
         value={props.maxYear}
         onChange={(e) => props.setMaxYear(e.target.value)}
         placeholder="Max Year"
-        className="movie-table-input"
+        className="movie-table-input year-input"
       />
       <input
         type="text"
@@ -37,14 +41,14 @@ const FilterControls = ({ ...props }) => {
         value={props.minRating}
         onChange={(e) => props.setMinRating(e.target.value)}
         placeholder="Min Rating"
-        className="movie-table-input"
+        className="movie-table-input rating-input"
       />
       <input
         type="number"
         value={props.maxRating}
         onChange={(e) => props.setMaxRating(e.target.value)}
         placeholder="Max Rating"
-        className="movie-table-input"
+        className="movie-table-input rating-input"
       />
     </div>
   );
